8d36c8af867ad74503b8018dfec6d4f02059227c
ilia_pepeliaev <iapepeliaev@gmail.com>
2023-02-05 19:27:03 +0200

    song-service postgres init configuration

---
+++ main-modul/song-service/src/main/resources/db/changelog/songs/ddl/001_create_tables.sql
@@ -0,0 +1,12 @@
+--liquibase formatted sql
+--changeset pepeliaevi:create-tables
+
+CREATE TABLE songs(
+   id INT PRIMARY KEY,
+   name VARCHAR(40),
+   artist VARCHAR(200),
+   album VARCHAR(200),
+   song_length VARCHAR(10),
+   resource_Id INT,
+   song_year VARCHAR(4)
+);
